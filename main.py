2bed8d13db256e11b597db4fd1bdcfd966776b8e
soonilovechu0409-png <soonilovechu0409@gmail.com>
2026-01-09 16:42:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!" , intents=intents)
+
+@bot.event
+async def on_ready():
+	print("봇이 온라인 상태입니다!")
+
+@bot.command()
+async def 노술게이(ctx):
+	await ctx.send("노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다 노술은 게이다")
+
+from datetime import timedelta
+import discord
+from discord.ext import commands
+
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def 타임아웃(ctx, member: discord.Member, minutes: int):
+    try:
+        await member.timeout(
+            timedelta(minutes=minutes),
+            reason=f"{ctx.author}에 의해 타임아웃"
+        )
+        await ctx.send(f"⏱️ {member.mention} 님을 {minutes}분 동안 타임아웃했어요.")
+    except Exception as e:
+        await ctx.send("❌ 타임아웃을 할 수 없어요.")
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def 타임아웃해제(ctx, member: discord.Member):
+    await member.timeout(None)
+    await ctx.send(f"✅ {member.mention} 님의 타임아웃을 해제했어요.")
+
+bot.run("MTQ1OTA1NDg4NDM1MDQ2MDAxNg.GNhDOD.95gPmB5mx7K5AETtSb2riAXcqBqGTGYhB5ip6U")
+
+import os
+bot.run(os.getenv("MTQ1OTA1NDg4NDM1MDQ2MDAxNg.GNhDOD.95gPmB5mx7K5AETtSb2riAXcqBqGTGYhB5ip6U")
